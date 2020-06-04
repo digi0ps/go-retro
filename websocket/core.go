c72ee8eaa05ff56c271214b9a4b63d95137ae60d
digi0ps <sriru1998@gmail.com>
2020-06-05 00:20:39 +0530

    Enable hub broadcasting.

--- websocket/core.go
+++ websocket/core.go
@@ -1,7 +1,6 @@
 package websocket
 
 import (
-	"encoding/json"
 	"fmt"
 	"go-retro/logger"
 	"net/http"
@@ -16,6 +15,35 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize:  1024,
 }
 
+type hub struct {
+	board string
+	users []*client
+}
+
+type client struct {
+	board *hub
+	id    string
+	conn  *websocket.Conn
+}
+
+func (c *client) close() {
+	index := -1
+
+	for i, user := range c.board.users {
+		if user.id == c.id {
+			index = i
+		}
+	}
+
+	c.board.users = append(c.board.users[:index], c.board.users[index+1:]...)
+
+	c.conn.Close()
+}
+
+var mainHub = &hub{
+	board: "main",
+}
+
 // InitHandler is responsible for initialising websocket handler
 func InitHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -27,31 +55,51 @@ func InitHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	defer conn.Close()
+	current := &client{
+		board: mainHub,
+		id:    fmt.Sprintf("%d", time.Now().Unix()),
+		conn:  conn,
+	}
+
+	fmt.Println(current)
+
+	mainHub.users = append(mainHub.users, current)
+
+	defer current.close()
 
 	for {
 		mt, message, err := conn.ReadMessage()
 		if err != nil {
 			logger.Error(err)
-			return
+			break
 		}
 
 		type wsmsg struct {
 			Name string `json:"name"`
 		}
 
-		var jsonMsg wsmsg
+		// var jsonMsg wsmsg
 
-		if err := json.Unmarshal(message, &jsonMsg); err != nil {
-			logger.Error(err)
-			return
-		}
+		// if err := json.Unmarshal(message, &jsonMsg); err != nil {
+		// 	logger.Error(err)
+		// 	return
+		// }
 
-		fmt.Printf("Message: %s, Type: %v", jsonMsg, mt)
+		fmt.Printf("Message: %s, Type: %v", message, mt)
 
-		if err := conn.WriteMessage(mt, message); err != nil {
-			logger.Error(err)
-			return
+		for _, userConn := range current.board.users {
+			if userConn.id == current.id {
+				continue
+			}
+
+			msg := fmt.Sprintf("user#%v: %s", current.id, message)
+
+			go func(conn *websocket.Conn, msg string) {
+				b := []byte(msg)
+				if err := conn.WriteMessage(mt, b); err != nil {
+					logger.Error(err)
+				}
+			}(userConn.conn, msg)
 		}
 	}
 
